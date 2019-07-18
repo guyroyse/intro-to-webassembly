b4f6aba3799179a10bd8f1f74d18de323c9baa98
Guy Royse <guy@guyroyse.com>
2019-07-18 10:08:54 -0500

    slightly smarter test

--- code/spec/fizzbuzz-spec.js
+++ code/spec/fizzbuzz-spec.js
@@ -1,83 +1,49 @@
 describe("FizzBuzz", () => {
 
-  describe("in WebAssembly Text Format", () => {
+  [
+    { module: '/fizzbuzz.wasm', description: "WebAssembly Text" },
+    { module: '/fizzbuzz.rs.wasm', description: "Rust" },
+  ].forEach(scenario => {
 
-    beforeEach(async done => {
-      this.subject = await loadWasm('/fizzbuzz.wasm')
-      done()
-    })
-  
-    it("returns the number it is given", () => {
-      expect(this.subject.fizzbuzz(1)).toBe(1)
-    })
-  
-    it("returns other numbers it is given", () => {
-      expect(this.subject.fizzbuzz(2)).toBe(2)
-    })
-  
-    it("returns -1 for multiples of 3", () => {
-      expect(this.subject.fizzbuzz(3)).toBe(-1)
-    })
-  
-    it("returns -1 for other multiples of 3", () => {
-      expect(this.subject.fizzbuzz(6)).toBe(-1)
-    })
-  
-    it("returns -2 for multiples of 5", () => {
-      expect(this.subject.fizzbuzz(5)).toBe(-2)
-    })
-  
-    it("returns -2 for other multiples of 5", () => {
-      expect(this.subject.fizzbuzz(10)).toBe(-2)
-    })
-  
-    it("returns -3 for multiples of 3 and 5", () => {
-      expect(this.subject.fizzbuzz(15)).toBe(-3)
-    })
-  
-    it("returns -3 for other multiples of 3 and 5", () => {
-      expect(this.subject.fizzbuzz(30)).toBe(-3)
-    })
-  
-  })
+    describe(scenario.description, () => {
 
-  describe("in Rust", () => {
-
-    beforeEach(async done => {
-      this.subject = await loadWasm('/fizzbuzz.rs.wasm')
-      done()
-    })
-  
-    it("returns the number it is given", () => {
-      expect(this.subject.fizzbuzz(1)).toBe(1)
-    })
-  
-    it("returns other numbers it is given", () => {
-      expect(this.subject.fizzbuzz(2)).toBe(2)
-    })
-  
-    it("returns -1 for multiples of 3", () => {
-      expect(this.subject.fizzbuzz(3)).toBe(-1)
-    })
-  
-    it("returns -1 for other multiples of 3", () => {
-      expect(this.subject.fizzbuzz(6)).toBe(-1)
-    })
-  
-    it("returns -2 for multiples of 5", () => {
-      expect(this.subject.fizzbuzz(5)).toBe(-2)
-    })
-  
-    it("returns -2 for other multiples of 5", () => {
-      expect(this.subject.fizzbuzz(10)).toBe(-2)
-    })
-  
-    it("returns -3 for multiples of 3 and 5", () => {
-      expect(this.subject.fizzbuzz(15)).toBe(-3)
-    })
-  
-    it("returns -3 for other multiples of 3 and 5", () => {
-      expect(this.subject.fizzbuzz(30)).toBe(-3)
+      beforeEach(async done => {
+        this.subject = await loadWasm(scenario.module)
+        done()
+      })
+    
+      it("returns the number it is given", () => {
+        expect(this.subject.fizzbuzz(1)).toBe(1)
+      })
+    
+      it("returns other numbers it is given", () => {
+        expect(this.subject.fizzbuzz(2)).toBe(2)
+      })
+    
+      it("returns -1 for multiples of 3", () => {
+        expect(this.subject.fizzbuzz(3)).toBe(-1)
+      })
+    
+      it("returns -1 for other multiples of 3", () => {
+        expect(this.subject.fizzbuzz(6)).toBe(-1)
+      })
+    
+      it("returns -2 for multiples of 5", () => {
+        expect(this.subject.fizzbuzz(5)).toBe(-2)
+      })
+    
+      it("returns -2 for other multiples of 5", () => {
+        expect(this.subject.fizzbuzz(10)).toBe(-2)
+      })
+    
+      it("returns -3 for multiples of 3 and 5", () => {
+        expect(this.subject.fizzbuzz(15)).toBe(-3)
+      })
+    
+      it("returns -3 for other multiples of 3 and 5", () => {
+        expect(this.subject.fizzbuzz(30)).toBe(-3)
+      })
+    
     })
   
   })
