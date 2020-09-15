220fb7db4438edd8fdcfb7ddd066b5b163171762
Guy Royse <guy@guyroyse.com>
2020-09-15 10:17:03 -0400

    cache busting

--- code/fizzbuzz/spec/wasm-helper.js
+++ code/fizzbuzz/spec/wasm-helper.js
@@ -1,5 +1,5 @@
 async function loadWasm(path) {
-  let response = await fetch(path)
+  let response = await fetch(path + '?x=' + Math.random())
   let bytes = await response.arrayBuffer()
   let module = await WebAssembly.instantiate(bytes)
   return module.instance.exports
